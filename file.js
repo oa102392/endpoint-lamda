55848eeb37d76c65edc623ce376def8b856d5b5d
oa102392 <oalba@csumb.edu>
2024-02-09 02:03:57 -0800

    -

--- file.js
+++ file.js
@@ -1,1 +1,23 @@
-//empty
+# Start from the official Apache Airflow image for version 2.8.3 with Python 3.11
+FROM apache/airflow:2.8.3-python3.11
+
+# Set user to root to make necessary changes
+USER root
+
+# Apply modifications for FIPS compliance
+# Modify Flask caching to use hashlib.sha256
+RUN sed -i "s/cache_config = {'CACHE_TYPE': 'flask_caching.backends.filesystem', \
+'CACHE_DIR': gettempdir()}/import hashlib\n    cache_config = \
+{'CACHE_TYPE': 'flask_caching.backends.filesystem', 'CACHE_DIR': \
+gettempdir(), 'CACHE_OPTIONS': {'hash_method': hashlib.sha256}}/g" \
+/home/airflow/.local/lib/python3.11/site-packages/airflow/www/app.py \
+ && find /home/airflow/.local/lib/python3.11/site-packages/airflow -type f \
+-exec sed -i "s/hashlib.md5/hashlib.sha256/g" {} \;
+
+# Now attempt to run apt-get update and any necessary installations
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    # list any packages you need to install here
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Switch back to the airflow user
+USER airflow
