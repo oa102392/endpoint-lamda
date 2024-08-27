b3b595663d0de09d7ca7479465b5e2abfbea722c
oa102392 <oalba@csumb.edu>
2024-08-27 13:13:25 -0700

    y

--- bars.tsx
+++ bars.tsx
@@ -1,31 +1,39 @@
-<Widget title="Project Technology Maturity" width={700}>
-    <BarChart
-        xAxis={[
-            {
-                id: 'trlLevels',
-                data: Object.keys(technology_maturity),
-                scaleType: 'band',
-                axisLabel: {
-                    formatter: (value) => `TRL ${value}`, // Label format
-                    rotate: 45, // Rotate X-axis labels
+// BarChartComponent.tsx
+'use client';
+
+import React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
+
+export default function BarChartComponent({ data }: { data: Record<string, number> }) {
+    return (
+        <BarChart
+            xAxis={[
+                {
+                    id: 'trlLevels',
+                    data: Object.keys(data),
+                    scaleType: 'band',
+                    axisLabel: {
+                        formatter: (value: string) => `TRL ${value}`, // Custom formatter
+                        rotate: 45, // Rotate X-axis labels for better readability
+                    },
                 },
-            },
-        ]}
-        yAxis={[
-            {
-                id: 'numberOfProjects',
-                title: 'Number of Projects',
-                axisLabel: {
-                    formatter: (value) => `${value}`, // Y-axis values
+            ]}
+            yAxis={[
+                {
+                    id: 'numberOfProjects',
+                    title: 'Number of Projects',
+                    axisLabel: {
+                        formatter: (value: number) => `${value}`, // Y-axis label formatter
+                    },
                 },
-            },
-        ]}
-        series={[
-            {
-                data: Object.values(technology_maturity),
-                barWidth: '20%', // Adjusts the bar width to make them slimmer
-            },
-        ]}
-        layout="vertical" // Keep vertical layout
-    />
-</Widget>
+            ]}
+            series={[
+                {
+                    data: Object.values(data),
+                    barWidth: '20%', // Set bar width to make them slimmer
+                },
+            ]}
+            layout="vertical" // Set the chart layout to vertical
+        />
+    );
+}
