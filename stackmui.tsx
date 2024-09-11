9fa05de8cbbc8c89b87ba6f8f39517f78aa18f35
oa102392 <oalba@csumb.edu>
2024-09-11 01:44:46 -0700

    q

--- stackmui.tsx
+++ stackmui.tsx
@@ -1,4 +1,7 @@
-import { BarChart } from '@mui/x-charts/BarChart';
+'use client';
+
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { Box } from '@mui/material';
 
 interface StackedBarChartComponentProps {
@@ -7,56 +10,44 @@ interface StackedBarChartComponentProps {
 
 export default function StackedBarChartComponent({ data }: StackedBarChartComponentProps) {
     return (
-        <Box sx={{ width: '100%', height: '500px', overflowY: 'scroll', position: 'relative' }}>
-
-            {/* Sticky X-Axis BarChart */}
+        <Box sx={{ width: '100%', height: '500px', position: 'relative' }}>
+            {/* X-axis bar chart */}
             <Box sx={{ position: 'sticky', top: 0, backgroundColor: '#fff', zIndex: 1 }}>
-                <BarChart
-                    dataset={data}
-                    width={800}
-                    height={60}  // X-axis only height
-                    xAxis={[
-                        {
-                            scaleType: 'linear',
-                            tickFormatter: (value) => `${(value / 1000000).toFixed(1)}M`,
-                            label: 'Funding (USD)',
-                        },
-                    ]}
-                    series={[]}  // Empty series since it's only for the X-axis
-                />
+                <ResponsiveContainer width="100%" height={50}>
+                    <BarChart
+                        width={800}
+                        height={60}
+                        data={[]}
+                    >
+                        <XAxis
+                            type="number"
+                            tickFormatter={(value) => `${(value / 1000000).toFixed(1)}M`}
+                            domain={[0, 'dataMax']}
+                            ticks={[0, 200000000, 500000000, 750000000, 1000000000, 1250000000]}
+                            interval={0}
+                            label={{ value: 'Funding (USD)', position: 'insideBottomRight', offset: -10 }}
+                        />
+                    </BarChart>
+                </ResponsiveContainer>
             </Box>
 
             {/* Scrollable bar chart */}
-            <BarChart
-                dataset={data}
-                xAxis={null}  // Remove X-axis from scrollable section
-                yAxis={[
-                    {
-                        scaleType: 'band',
-                        dataKey: 'name', // Site or program names
-                        label: 'Programs or Sites',
-                        tickLabelPlacement: 'middle',
-                    },
-                ]}
-                series={[
-                    {
-                        dataKey: 'lifeCycleCost',
-                        label: 'Project Life Cycle Cost',
-                        stack: 'total',
-                        barSize: 10,
-                    },
-                    {
-                        dataKey: 'fy2024Target',
-                        label: 'Total Target FY 2024 Req',
-                        stack: 'total',
-                        barSize: 15,  // Example of varying size for the stacked bar
-                    },
-                ]}
-                layout="horizontal"
-                width={800}
-                height={data.length * 40}  // Dynamic height based on data
-                grid={{ vertical: true }}
-            />
+            <Box sx={{ maxHeight: '450px', overflowY: 'scroll' }}>
+                <ResponsiveContainer width="100%" height={data.length * 40}>
+                    <BarChart
+                        data={data}
+                        layout="vertical"
+                        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <YAxis type="category" dataKey="name" />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="lifeCycleCost" stackId="a" fill="#d3b8e4" name="Project Life Cycle Cost" />
+                        <Bar dataKey="fy2024Target" stackId="a" fill="#6a0dad" name="Total Target FY 2024 Req" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </Box>
         </Box>
     );
 }
