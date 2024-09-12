3b7c9ac5115bc171450f2b6762ad599b622d4979
oa102392 <oalba@csumb.edu>
2024-09-12 01:30:29 -0700

    1

---
+++ statbar3.tsx
@@ -0,0 +1,49 @@
+// StackedBarChartComponent.tsx
+'use client';
+
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+interface StackedBarChartComponentProps {
+    data: { name: string; lifeCycleCost: number; fy2024Target: number }[];
+    title: string;
+}
+
+export default function StackedBarChartComponent({ data, title }: StackedBarChartComponentProps) {
+    return (
+        <div style={{ textAlign: 'center', margin: '20px' }}>
+            <h3>{title}</h3>
+            
+            <div style={{ maxHeight: '450px', overflowY: 'scroll', position: 'relative' }}>
+                <ResponsiveContainer width="100%" height={data.length * 40}>
+                    <BarChart
+                        data={data}
+                        margin={{ top: 10, right: 30, left: 50, bottom: 10 }}
+                        layout="vertical"
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        
+                        {/* XAxis */}
+                        <XAxis 
+                            type="number" 
+                            tickFormatter={(value) => `$${(value / 1000000).toFixed(0)}M`} 
+                            domain={[0, 'dataMax']} 
+                            interval={0} 
+                            ticks={[0, 200000000, 500000000, 750000000, 1000000000]} 
+                        />
+
+                        {/* YAxis */}
+                        <YAxis dataKey="name" type="category" tick={{ textAnchor: 'end' }} tickMargin={10} interval={0} />
+                        
+                        <Tooltip />
+                        <Legend />
+
+                        {/* Bars */}
+                        <Bar dataKey="lifeCycleCost" stackId="a" fill="#d3b8e4" name="Project Life Cycle Cost" />
+                        <Bar dataKey="fy2024Target" stackId="a" fill="#6a0dad" name="Total Target FY 2024 Req" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+        </div>
+    );
+}
