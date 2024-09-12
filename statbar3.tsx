553dff228963b550247511cf263e7c4628e788de
oa102392 <oalba@csumb.edu>
2024-09-12 01:34:35 -0700

    1

--- statbar3.tsx
+++ statbar3.tsx
@@ -1,9 +1,9 @@
-// StackedBarChartComponent.tsx
 'use client';
 
 import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
+// Interface for the data prop
 interface StackedBarChartComponentProps {
     data: { name: string; lifeCycleCost: number; fy2024Target: number }[];
     title: string;
@@ -13,28 +13,27 @@ export default function StackedBarChartComponent({ data, title }: StackedBarChar
     return (
         <div style={{ textAlign: 'center', margin: '20px' }}>
             <h3>{title}</h3>
-            
-            <div style={{ maxHeight: '450px', overflowY: 'scroll', position: 'relative' }}>
+
+            {/* Container for the chart with a scrollable Y-axis */}
+            <div style={{ position: 'relative', height: '450px', overflowY: 'scroll' }}>
                 <ResponsiveContainer width="100%" height={data.length * 40}>
                     <BarChart
                         data={data}
-                        margin={{ top: 10, right: 30, left: 50, bottom: 10 }}
                         layout="vertical"
+                        margin={{ top: 10, right: 30, left: 50, bottom: 50 }} // Leave space for X-axis at bottom
                     >
                         <CartesianGrid strokeDasharray="3 3" />
                         
-                        {/* XAxis */}
-                        <XAxis 
-                            type="number" 
-                            tickFormatter={(value) => `$${(value / 1000000).toFixed(0)}M`} 
-                            domain={[0, 'dataMax']} 
-                            interval={0} 
-                            ticks={[0, 200000000, 500000000, 750000000, 1000000000]} 
+                        {/* YAxis (scrollable) */}
+                        <YAxis
+                            dataKey="name"
+                            type="category"
+                            tick={{ textAnchor: 'end' }}
+                            tickMargin={10}
+                            interval={0}
                         />
 
-                        {/* YAxis */}
-                        <YAxis dataKey="name" type="category" tick={{ textAnchor: 'end' }} tickMargin={10} interval={0} />
-                        
+                        {/* Tooltip */}
                         <Tooltip />
                         <Legend />
 
@@ -43,6 +42,28 @@ export default function StackedBarChartComponent({ data, title }: StackedBarChar
                         <Bar dataKey="fy2024Target" stackId="a" fill="#6a0dad" name="Total Target FY 2024 Req" />
                     </BarChart>
                 </ResponsiveContainer>
+
+                {/* Fixed XAxis */}
+                <div
+                    style={{
+                        position: 'sticky',
+                        bottom: 0,
+                        backgroundColor: '#fff',
+                        zIndex: 100,
+                        width: '100%',
+                    }}
+                >
+                    <ResponsiveContainer width="100%" height={60}>
+                        <BarChart data={data} layout="horizontal">
+                            <XAxis
+                                type="number"
+                                tickFormatter={(value) => `$${(value / 1000000).toFixed(0)}M`}
+                                domain={[0, 'dataMax']}
+                                ticks={[0, 200000000, 500000000, 750000000, 1000000000]}
+                            />
+                        </BarChart>
+                    </ResponsiveContainer>
+                </div>
             </div>
         </div>
     );
